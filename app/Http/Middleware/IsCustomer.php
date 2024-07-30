531c1805148e256a518a67c4d99a655336c498c5
Edy Susilo <masedysu69@gmail.com>
2024-07-30 12:00:45 +0700

    update route

--- app/Http/Middleware/IsCustomer.php
+++ app/Http/Middleware/IsCustomer.php
@@ -1,20 +1,20 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class IsCustomer
 {
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role === 'customer') {
+        if (Auth::check() && Auth::user()->role == 'customer') {
             return $next($request);
         }
 
-        return redirect()->route('login')->with('error', 'You do not have access to this area.');
+        return $request->expectsJson()
+            ? abort(403, 'Unauthorized')
+            : redirect()->back()->withErrors(['access' => 'You do not have access to this page.']);
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,25 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-// routes/web.php
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CustomerController;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return redirect()->route('check');
+});
+
+Route::get('/check', function () {
+    if (Auth::check()) {
+        $user = Auth::user();
+        if ($user->role == 'admin') {
+            return redirect()->route('admin.dashboard');
+        } elseif ($user->role == 'customer') {
+            return redirect()->route('customer.dashboard');
+        }
+    }
+    return redirect()->route('login');
+})->name('check');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
